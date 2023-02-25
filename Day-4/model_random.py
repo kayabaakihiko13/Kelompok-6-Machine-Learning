5bc16e84dc3c4933c88d38b25f8b8e0b1f05a33b
kayabaakihiko13 <iqbalramad75@gmail.com>
2023-02-25 09:02:47 +0700

    add:menambah algoritma baru

--- Day-4/model_random.py
+++ Day-4/model_random.py
@@ -1,7 +1,6 @@
-
 import time
-import numpy as np
 import os
+import numpy as np
 """
 
 ===========================
@@ -9,80 +8,102 @@ import os
 ===========================
 
 """
-BPF = 53  
-def random():
+
+
+def random(size=1):
     """
-    random ini berfungsi untuk menampilkan angka dengan sesuai pseucode 
-    ini 
+    angka acak ini menggunakan basis LCG Linear congruential generator
+    refence:
+
+    https://en.wikipedia.org/wiki/Linear_congruential_generator
+
     >>> random()
     0.153196923667565
     >>> random()
     0.734023273922503
     """
-    t=int(time.time()*1000)
-    x=t%(2**32)
-    while True:
+    t = int(time.time() * 1000)
+    x = t % (2 ** 31)
+    memo=[]
+    for _ in range(size):
+        x = (1664525 * x + 12345) % (2 ** 31)
+        hitung = x / (2 ** 31)
+        memo.append(hitung)
+    return np.array(memo)
 
-        x=(1664525*x+12345)%(2**32)
-        return x/(2**32)
+def seed(number):
+    def generator(size=None):
+        t=seed
+        x = t % (2 ** 31)
+        memo=[]
+        while size is None | len(memo)<size:
+            x = (1664525 * x + 12345) % (2 ** 31)
+            hitung=x/(2*31)
+            memo.append(hitung)
+        if size is None:
+            memo[0]
+        return memo
+    return random
 
 
 def getrandbits(k):
     """
     konsep dari getrandbits ini menggunakan bits acak yang relative
-    dengan melewati dua waktu
-    refence:
-    https://en.wikipedia.org/wiki/Linear_congruential_generator
+    dengan melewati dua waktu atau dengan menggunakan generator byte
 
     """
-    bytes_need=(k+7)
-    randbytes=os.urandom(bytes_need)
-    randbit=int.from_bytes(randbytes,byteorder='big')
-    return randbit>>(bytes_need*8-k)
+    bytes_need = (k + 7) // 8
+    randbytes = os.urandom(bytes_need)
+    randbit = int.from_bytes(randbytes, byteorder='big')
+    return randbit >> (bytes_need*8-k)
 
 
-# make important 
-def randbelow(n):
-    """
-    
-    """
-    if n<=0:
+def generator_bit(n):
+    if n <= 0:
         raise ValueError("the input cant be less from zero")
-    k=n.bit_length()        
+    k = n.bit_length()
     return getrandbits(k) % n
 
 
-def randomint(a,b,size=1):
-    result=[]
+def randomint(a, b, size=1):
+    result = []
     for _ in range(size):
-        result.append(randbelow(b-a+1))
-    return np.array(result)
+        result.append(generator_bit(b - a + 1) + a)
+    return np.array(result).reshape(-1,1)
 
 
-def randomrange(a,b,size):
-    result=[]
+def randomrange(a, b, size=1):
+    result = []
     for _ in range(size):
-        result.append(randbelow(b-a))
-    return np.array(result)
+        result.append(generator_bit(b - a) + a)
+    return np.array(result).shape(-1,1)
+
+def randuni(low=0,high=1,size=1):
+    memo=np.array([])
+    for _ in range(size):
+        hitung=low+(high-low)*random()
+        memo=np.append(memo,hitung)
+    return memo.shape(-1,1)
+
+def generate_dummy_data_single(n_samples, beta_0, beta_1, noise_scale):
+    x = np.linspace(0, 1, n_samples)
+    noise = randuni(size=n_samples, low=-noise_scale, high=noise_scale)
+    y = beta_0 + beta_1 * x + noise
+    return x, y
 
 
-def random_uniform(size=1):
-    if size<=0:
-        raise ValueError("Can't put size value lower than 1")
-    
-    memo=[]
-    for _ in range(size): 
-        memo.append(0 + (1 - 0) * random())
-    return np.array(memo)
+"""
+import random
 
-## Tahap pembagunan
-# def random_uniform(low,high,size=1):
-#     result=np.array([])
-#     for _ in range(size):
-#         result=np.append(result,low+(high-low)*randomint(low,high,size=1))
-#     return result
+def generate_random_float(lower_bound, upper_bound):
+    return random.random() * (upper_bound - lower_bound) + lower_bound
 
-if __name__=="___main__":
+random_float = generate_random_float(0.0, 1.0)
+print(random_float)
+
+"""
+
+
+if __name__ == "__main__":
     import doctest
-
     doctest.testmod(verbose=True)
